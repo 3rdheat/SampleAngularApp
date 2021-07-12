a9340723b713110f776091e471dedfaed68db867
jimsonrojo1437@gmail.com <jimsonrojo1437@gmail.com>
2021-07-12 17:35:24 +0800

    added interface and content for recipe list

--- my-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipes.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Adobong manac","chicken with soy sauce and vinegar","https://upload.wikimedia.org/wikipedia/commons/3/38/Chicken_adobo.jpg"),
+    new Recipe("Sinigang","super kilig","https://th.bing.com/th/id/R.faa347dd003c8058c8d126194d8c90d6?rik=uLzoRo2s8%2bLibQ&riu=http%3a%2f%2fwww.angsarap.net%2fwp-content%2fuploads%2f2014%2f01%2fsinigang-na-baboy-wide.jpg&ehk=pYkg73qwrPyDAM4pGcjk5N5ivhgjUSdfxVoU3r53zkc%3d&risl=&pid=ImgRaw")
+  ];
+
 
   constructor() { }
 
