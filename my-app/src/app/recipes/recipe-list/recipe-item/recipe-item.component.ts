616b49bfa1a6b74b9abcee96b183dc9893be1eed
jimsonrojo1437@gmail.com <jimsonrojo1437@gmail.com>
2021-07-19 17:53:40 +0800

    adding recipe service

--- my-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ my-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { RecipeService } from '../../recipe.service';
 import { Recipe } from '../../recipes.model';
 
 @Component({
@@ -10,12 +11,14 @@ export class RecipeItemComponent implements OnInit {
 
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
   }
 
-  
+  onSelected(){
+    this.recipeService.recipeSelected.emit(this.recipe);
+  }
 
 
 }
